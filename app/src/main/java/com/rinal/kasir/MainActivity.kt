6b9ffe78d8485f9d64b7c608a40983be2a92b43d
Muh Afrinal <afrinalhakim29@gmail.com>
2020-01-27 20:28:45 +0700

    update navigation drawer

--- app/src/main/java/com/rinal/kasir/MainActivity.kt
+++ app/src/main/java/com/rinal/kasir/MainActivity.kt
@@ -42,8 +42,7 @@ class MainActivity : AppCompatActivity() {
 
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_dashboard, R.id.nav_profile ,R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
-                R.id.nav_tools, R.id.nav_share, R.id.nav_send
+                R.id.nav_dashboard, R.id.nav_profile, R.id.nav_transaction
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
---
+++ app/src/main/java/com/rinal/kasir/ui/transaksi/TransactionFragment.kt
@@ -0,0 +1,22 @@
+package com.rinal.kasir.ui.transaksi
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.rinal.kasir.R
+
+class TransactionFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_transaction, container, false)
+    }
+
+
+}
