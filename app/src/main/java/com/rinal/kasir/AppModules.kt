4643c49c1649156a08e779101343914207ed1100
Muh Afrinal <afrinalhakim29@gmail.com>
2020-01-28 11:57:19 +0700

    koin konfigurasi

--- app/src/main/java/com/rinal/kasir/AppModules.kt
+++ app/src/main/java/com/rinal/kasir/AppModules.kt
@@ -1,2 +1,12 @@
 package com.rinal.kasir
 
+import com.rinal.kasir.db.Databases
+import com.rinal.kasir.model.MainViewModel
+import org.koin.android.ext.koin.androidContext
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val appModule = module {
+    single { Databases.getInstance(androidContext()) }
+    viewModel { MainViewModel(get()) }
+}
